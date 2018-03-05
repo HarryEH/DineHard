a72ed4d1d86a0b829e88887f7f345493eab4ec7b
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-03-05 21:47:09 +0000

    started using mongoose

---
+++ project/routes/models.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mydb');
+
+const Cat = mongoose.model('Cat', { name: String});
+
+const kitty = new Cat({ name: 'Zildjian' });
+
+kitty.save().then(() => console.log('meow'));
+
+
