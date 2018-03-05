75cad26fb5fcaa0f7beabbb3d38e00f189587d36
Adam Orr <aorr1@sheffield.ac.uk>
2018-03-05 16:55:23 +0000

    Results page

--- project/routes/index.js
+++ project/routes/index.js
@@ -14,4 +14,9 @@ router.get('/restaurant-*', function(req, res, next) {
     res.render('restaurant', { loggedIn: true });
 });
 
+router.get('/results', function(req, res, next) {
+    var results = ["Restaurant 1", "Restaurant 2", "Restaurant 3"];
+    res.render('results', { loggedIn: true, results: results  });
+});
+
 module.exports = router;
