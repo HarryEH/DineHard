b393d04f5a7e09924e5fa49c83b8d55a2fec25b9
Adam Orr <aorr1@sheffield.ac.uk>
2018-03-06 09:20:24 +0000

    Results page update

--- project/public/javascripts/error.js
+++
@@ -1,7 +0,0 @@
-function errorCheck(errorStatus){
-    var errorTitle = "";
-    if(errorStatus == "404"){
-        errorTitle = "<h1>Whoopsies! This page doesn't seem to exist!</h1>";
-    }
-    document.getElementById('errorTitle').innerHTML = errorTitle;
-}
