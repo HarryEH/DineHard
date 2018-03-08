7e89b6da940edc6fad46101089df794d0863dd55
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-03-08 16:40:33 +0000

    cleaner code

--- project/routes/ResultsController.js
+++ project/routes/ResultsController.js
@@ -17,59 +17,62 @@ module.exports = {
 
         // undefined || empty - return everything
         if (typeof str == 'undefined') {
-            console.log('first');
-            models.Restaurant.find(function (err, results) {
-                if (err) {return console.error(err);}
-
-                console.log(results);
-
-                res.render('results', { loggedIn: login, results: results  });
-
-            });
+            returnAll(res);
             return;
         }
 
         // undefined || empty - return everything
         if (str == "") {
-            console.log('second');
-            models.Restaurant.find(function (err, results) {
-                if (err) {return console.error(err);}
-
-                console.log(results);
-
-                res.render('results', { loggedIn: login, results: results  });
-
-            });
+            returnAll(res);
             return;
         }
 
         // if postcode
         if (valid_postcode(str)) {
-            console.log('third');
-            // do something
-            console.log("valid_postcode");
-            // get the lat lng of the postcode entered
-            // find the right size bubble around it
-            var lat_min = 54.8;
-            var lat_max = 55;
-            var lng_min = -1.7;
-            var lng_max = -1.5;
-
-            models.connect();
-
-            models.Restaurant.find({lat: { $gt: lat_min, $lt: lat_max }, lng: { $gt: lng_min, $lt: lng_max }}, function (err, results) {
-                if (err) {return console.error(err);}
-
-                console.log(results);
-
-                res.render('results', { loggedIn: login, results: results  });
-
-            });
+            onPostcode(res);
             return;
         }
 
         //res.render('results', { loggedIn: login, results: [{name:"res1"}, {name:"res2"}, {name:"res3"} ]  });
     }
+};
+
+function onPostcode(res){
+
+    console.log('third');
+    // do something
+    console.log("valid_postcode");
+    // get the lat lng of the postcode entered
+    // find the right size bubble around it
+    var lat_min = 54.8;
+    var lat_max = 55;
+    var lng_min = -1.7;
+    var lng_max = -1.5;
+
+    models.connect();
+
+    models.Restaurant.find({lat: { $gt: lat_min, $lt: lat_max }, lng: { $gt: lng_min, $lt: lng_max }}, function (err, results) {
+        if (err) {return console.error(err);}
+
+        console.log(results);
+
+        res.render('results', { loggedIn: login, results: results  });
+
+    });
+
+};
+
+function returnAll(res){
+    console.log('all');
+
+    models.Restaurant.find(function (err, results) {
+        if (err) {return console.error(err);}
+
+        console.log(results);
+
+        res.render('results', { loggedIn: login, results: results  });
+
+    });
 }
 
 
