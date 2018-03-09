ec3ac924ae0563dbf2cdd2e98ac685e1a759c785
Adam Orr <aorr1@sheffield.ac.uk>
2018-03-09 12:39:47 +0000

    Address to Lat Long implemented

--- project/public/javascripts/location.js
+++ project/public/javascripts/location.js
@@ -26,3 +26,17 @@ var getDistance = function(p1, p2) {
     var d = R * c;
     return d; // returns the distance in meter
 };
+
+function addressToLocation(address){
+
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+            alert(latitude);
+            alert(longitude);
+        }
+    })
+}
