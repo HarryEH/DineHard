8953e7fc6894a9fb62deebeeb2293b98444968af
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-03-19 21:58:58 +0000

    google maps shit done

--- project/public/javascripts/location.js
+++ project/public/javascripts/location.js
@@ -21,3 +21,41 @@ function setPosition(position) {
     sessionStorage.setItem("userLat", position.coords.latitude);
     sessionStorage.setItem("userLong", position.coords.longitude);
 }
+
+function myMap(){
+    const mapOptions = {
+        center: new google.maps.LatLng(0, 0),
+        zoom: 17,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: false,
+    };
+    const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+function addMarkers(lat,lng) {
+
+    const mapOptions = {
+        center: new google.maps.LatLng(lat, lng),
+        zoom: 17,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: false,
+    };
+    const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    const marker = new google.maps.Marker({
+        position: {lat: lat, lng: lng},
+        map: map,
+        title: 'Restaurant'
+    });
+
+    map.addMarker(marker);
+
+    map.addMarker(lat, lng);
+
+}
