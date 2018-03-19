518879d183f6f532ccd317c25f8eb99a9abcc51e
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-03-19 20:19:46 +0000

    most of the work for resetting password done

--- project/controllers/passwordcontroller.js
+++ project/controllers/passwordcontroller.js
@@ -3,33 +3,46 @@ const models = require('../models/models');
 module.exports = {
 
     sendEmail: function (req, res, login) {
-        // Hit Db to get user's email
-        // Send them an email
+        models.User.findOne({ username: new RegExp(req.body.username, "i") }, function (err, user) {
+            if (err) return console.error(err);
 
-        console.log(req.body.username);
+            const url = "http://"+ req.get('host') +"/change-password?username="+ user.username +"&tokenId="+ user._id;
+            console.log(url);//KEEP THIS
 
-        
-        res.mailer.send('email', {
-            to: 'example@example.com', // REQUIRED. This can be a comma delimited string just like a normal email to field.
-            subject: 'Test Email', // REQUIRED.
-            otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables.
-        }, function (err) {
-            if (err) {
-                // handle error
-                console.log(err);
-                res.send('There was an error sending the email');
-                return;
-            }
-
-            res.render('forgot-password', {loggedIn: login, error: "Email Sent. Check your Emails"});
+            // Send them an email
+            req.app.mailer.send('email', {
+                to: user.email,
+                subject: 'Reset Password',
+                unique_url: url,
+                username: user.username
+            }, function (err) {
+                if (err) {
+                    // handle error
+                    res.render('forgot-password', {loggedIn: login, error: 'There was an error sending the email'});
+                    return;
+                }
+                res.render('forgot-password', {loggedIn: login, error: "Email Sent. Check your Emails"});
+            });
         });
-
 
     },
 
     handleReset: function (req, res, login) {
+
         models.connect();
-        // verify that the restaurant's address isn't already in the db
+
+        // Verify things then update
+
+        // Example code
+        // models.User.update({username: oldUsername}, {
+        //     username: newUser.username,
+        //     password: newUser.password,
+        //     rights: newUser.rights
+        // }, function(err, numberAffected, rawResponse) {
+        //     //handle it
+        // });
+
+        res.redirect('/login');
 
     }
 
