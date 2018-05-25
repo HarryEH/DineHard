68e08ca2c061f75dbcb24dca3aa994312e9474bb
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-05-25 18:49:16 +0100

    service workers now work.

--- project/public/service-worker.js
+++ project/public/service-worker.js
@@ -3,11 +3,8 @@ const ERROR_CACHE = 'dine-hard-error';
 const urlsToCache = [
 // add the files you want to cache here
     '/',
-    '/index',
-    '/stylesheets/*',
-    '/javascripts/*',
-    '/results*',
-    '/*'
+    '/stylesheets/style.css',
+    '/offline.html'
 ];
 
 self.addEventListener('install', function(event) {
@@ -23,79 +20,77 @@ self.addEventListener('install', function(event) {
 
 });
 
-// self.addEventListener('activate', function(event){
-//    console.log('[ServiceWorker] Activate');
-//    e.waitUntil(
-//        caches.keys().then(function(keyList) {
-//            return Promise.all(keyList.map(function(key){
-//                if(key !== cacheName && key !== dataCacheName) {
-//                    console.log('[ServiceWorker] Removing old cache', key);
-//                    return caches.delete(key);
-//                }
-//            }))
-//        })
-//    )
-// });
+self.addEventListener('activate', function(event){
+    console.log('[ServiceWorker] Activate');
 
-// self.addEventListener('fetch', function(event) {
-//
-//     event.respondWith(
-//         caches.match(event.request)
-//             .then(function(response) {
-//                 if (response) {
-//                     return response;     // if valid response is found in cache return it
-//                 } else {
-//                     return fetch(event.request)     //fetch from internet
-//                         .then(function(res) {
-//                             return caches.open(CACHE_NAME)
-//                                 .then(function(cache) {
-//                                     cache.put(event.request.url, res.clone());    //save the response for future
-//                                     return res;   // return the fetched data
-//                                 })
-//                         })
-//                         .catch(function(err) {       // fallback mechanism
-//                             return caches.open(ERROR_CACHE)
-//                                 .then(function(cache) {
-//                                     return cache.match('/offline.html');
-//                                 });
-//                         });
-//                 }
-//             })
-//     );
-// });
+    event.waitUntil(
+       caches.keys().then(function(keyList) {
+           return Promise.all(keyList.map(function(key){
+               if(key !== CACHE_NAME) {
+                   console.log('[ServiceWorker] Removing old cache', key);
+                   return caches.delete(key);
+               }
+           }))
+       })
+    )
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request);
+    event.respondWith (
+        fetch(event.request).catch(function(e) {
+            console.error('Fetch failed; returning a cached or offline page instead.', e);
+            return caches.open(CACHE_NAME).then(function(cache) {
+                //return cache.match(event.request);
+                return caches.match(event.request).then(function (response) {
+                    // Cache hit - return the cache response (the cached page)
+                    if (response) {
+                        console.log("here1");
+                        return response;
+                    } else {
+                        console.log("here2");
+                        return caches.match('/offline.html').then(function(r){return r;})
+                    }
+                })
+            });
+        })
+        // fetch(event.request).catch(function(err) {
+        //     // it checks if the requested page is among the cached ones
+        //     caches.match(event.request).then(function (response) {
+        //         // Cache hit - return the cache response (the cached page)
+        //         if (response) {
+        //             console.log("here1");
+        //             return response;
+        //         } else {
+        //             console.log("here2");
+        //             return caches.match('/offline.html').then(function(r){return r;})
+        //         }
+        //     })
+        // })
+    );
+
+    var fetchRequest = event.request.clone();
+    return fetch(fetchRequest).then( function (response) {
+        // Check if we received a valid response. A basic response is one that
+        // is made to our own site. Do not cache responses to requests made
+        // to other sites
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+        }
+        // IMPORTANT: as mentioned we must clone the response.
+        // A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                cache.put(event.request, responseToCache);
+            });
+        return response;
+    });
+});
 
 
 
-//
-// console.log(event.request);
-// event.respondWith(
-//     // it checks if the requested page is among the cached ones
-//     caches.match(event.request).then(function (response) {
-//         // Cache hit - return the cache response (the cached page)
-//         if (response) {
-//             return response;
-//         } //cache does not have the page — go to the server
-//         return fetch(event.request);
-//     })
-// );
-// var fetchRequest = event.request.clone();
-// return fetch(fetchRequest).then( function (response) {
-//     // Check if we received a valid response. A basic response is one that
-//     // is made to our own site. Do not cache responses to requests made
-//     // to other sites
-//     if (!response || response.status !== 200 || response.type !== 'basic') {
-//         return response;
-//     }
-//     // IMPORTANT: as mentioned we must clone the response.
-//     // A response is a stream
-//     // and because we want the browser to consume the response
-//     // as well as the cache consuming the response, we need
-//     // to clone it so we have two streams.
-//     var responseToCache = response.clone();
-//     caches.open(CACHE_NAME)
-//         .then(function (cache) {
-//             cache.put(event.request, responseToCache);
-//         });
-//     return response;
-// });
 
