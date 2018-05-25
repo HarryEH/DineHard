bfcdf543b3c7a440a2c381645c59662b39dcff55
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-05-25 20:39:42 +0100

    erm wtf

--- project/public/service-worker.js
+++ project/public/service-worker.js
@@ -36,60 +36,83 @@ self.addEventListener('activate', function(event){
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log(event.request);
+
+    const cunt = event.request.clone();
+    // console.log(event.request);
+    //
+    // event.request.arrayBuffer().then(function(r){
+    //     var enc = new TextDecoder("utf-8");
+    //     var arr = new Uint8Array(r);
+    //     console.log(enc.decode(arr));
+    // });
+    //
+    // console.log(event.request);
+
+
     event.respondWith (
-        fetch(event.request).catch(function(e) {
+        fetch(event.request).then( function (response) {
+            if (event.request.method === "GET") {
+                if (response && response.status === 200 && response.type === 'basic') {
+                    var responseToCache = response.clone();
+                    caches.open(CACHE_NAME).then(function (cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+                }
+
+            } else {
+                console.log(cunt);
+
+                // event.request.arrayBuffer()
+                // event.request.blob()
+                // event.request.json()
+                // event.request.text()
+                // event.request.formData()
+
+                // cunt.arrayBuffer().then(function(r){
+                //     var enc = new TextDecoder("utf-8");
+                //     var arr = new Uint8Array(r);
+                //     console.log(enc.decode(arr));
+                // });
+
+                cunt.text().then(function(r){
+                    console.log(r);
+                }).catch(function(err){console.log(err)});
+            }
+            return response;
+        }).catch(function(e) {
             console.error('Fetch failed; returning a cached or offline page instead.', e);
             return caches.open(CACHE_NAME).then(function(cache) {
-                //return cache.match(event.request);
                 return caches.match(event.request).then(function (response) {
                     // Cache hit - return the cache response (the cached page)
                     if (response) {
-                        console.log("here1");
                         return response;
                     } else {
-                        console.log("here2");
                         return caches.match('/offline.html').then(function(r){return r;})
                     }
                 })
             });
         })
-        // fetch(event.request).catch(function(err) {
-        //     // it checks if the requested page is among the cached ones
-        //     caches.match(event.request).then(function (response) {
-        //         // Cache hit - return the cache response (the cached page)
-        //         if (response) {
-        //             console.log("here1");
-        //             return response;
-        //         } else {
-        //             console.log("here2");
-        //             return caches.match('/offline.html').then(function(r){return r;})
-        //         }
-        //     })
-        // })
     );
 
-    var fetchRequest = event.request.clone();
-    return fetch(fetchRequest).then( function (response) {
-        // Check if we received a valid response. A basic response is one that
-        // is made to our own site. Do not cache responses to requests made
-        // to other sites
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-        }
-        // IMPORTANT: as mentioned we must clone the response.
-        // A response is a stream
-        // and because we want the browser to consume the response
-        // as well as the cache consuming the response, we need
-        // to clone it so we have two streams.
-        var responseToCache = response.clone();
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                cache.put(event.request, responseToCache);
-            });
-        return response;
-    });
+    // if (event.request.method === "GET") {
+    //     var fetchRequest = event.request.clone();
+    //     return fetch(fetchRequest).then( function (response) {
+    //
+    //         if (!response || response.status !== 200 || response.type !== 'basic') {
+    //             return response;
+    //         }
+    //
+    //         var responseToCache = response.clone();
+    //         caches.open(CACHE_NAME)
+    //             .then(function (cache) {
+    //                 cache.put(event.request, responseToCache);
+    //             });
+    //         return response;
+    //     });
+    // }
+
 });
+
 
 
 
