9556b551ce35390eceb74cc690a28d6bf567c79e
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-05-31 13:44:01 +0100

    FUCK. FIX FIX FIX

--- project/public/javascripts/write-review-functionality.js
+++ project/public/javascripts/write-review-functionality.js
@@ -47,32 +47,36 @@ function ajax(photoData) {
  * @param data this is the data sent in the broadcasted message.
  */
 function handleSocket(data){
-    document.getElementById('updateable').outerHTML = data.rendered;
-    // The <script> doesn't get run so this is required to do that
 
-    const x = data.restaurant.rating;
-    var y = data.restaurant.noRating;
+    if (document.URL.includes(data.restaurant._id)) {
+        document.getElementById('updateable').outerHTML = data.rendered;
+        // The <script> doesn't get run so this is required to do that
 
-    if(y == 0) {
-        y = 1;
+        const x = data.restaurant.rating;
+        var y = data.restaurant.noRating;
+
+        if(y == 0) {
+            y = 1;
+        }
+
+        getStarRating(x/y, "starRating");
+
+        if (document.getElementById('no-reviews-yet') !== null) {
+            document.getElementById('no-reviews-yet').style.display = 'none';
+        }
+
+        document.getElementById("ratingText").innerText = Math.round((x/y) * 100) / 100 + " with " + y + " reviews";
+
+        for (var i = 0; i < data.results.length; i++) {
+            var divID = "review-star" + (i+1).toString();
+            getStarRating(data.results[i].rating, divID);
+            divID = "review-date" + (i+1).toString();
+            const date = new Date(data.results[i].date);
+            formatDate(date.getDate(),date.getMonth(), date.getFullYear(), divID);
+
+        }
     }
 
-    getStarRating(x/y, "starRating");
-
-    if (document.getElementById('no-reviews-yet') !== null) {
-        document.getElementById('no-reviews-yet').style.display = 'none';
-    }
-
-    document.getElementById("ratingText").innerText = Math.round((x/y) * 100) / 100 + " with " + y + " reviews";
-
-    for (var i = 0; i < data.results.length; i++) {
-        var divID = "review-star" + (i+1).toString();
-        getStarRating(data.results[i].rating, divID);
-        divID = "review-date" + (i+1).toString();
-        const date = new Date(data.results[i].date);
-        formatDate(date.getDate(),date.getMonth(), date.getFullYear(), divID);
-
-    }
 }
 
 /**
